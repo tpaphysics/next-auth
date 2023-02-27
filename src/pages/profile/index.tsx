16a95c1c3740b09cbfeeee4208b6ca02133dc3fd
chewacca <physics.posgrad@gmail.com>
2023-02-26 21:30:15 -0300

    style: estilização do profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -70,29 +70,16 @@ export default function Profile() {
               <div className="px-6">
                 <div className="flex flex-wrap justify-center">
                   <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                    <div className="relative">
-                      <img
-                        alt="..."
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                        src={image}
-                        style={{ maxWidth: "150px" }}
-                      />
-                    </div>
+                    <img
+                      alt="..."
+                      className="shadow-xl rounded-full h-32 w-32 align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                      src={image}
+                      style={{ maxWidth: "150px" }}
+                    />
                   </div>
-                  <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                    <div className="py-6 px-3 sm:mt-0">
-                      <button
-                        onClick={() => handleSignOut()}
-                        className="bg-purple-700 active:bg-purple-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:m-2 mb-1 hover:bg-purple-700/90"
-                        type="button"
-                        style={{ transition: "all .15s ease" }}
-                      >
-                        Sign out
-                      </button>
-                    </div>
-                  </div>
+                  <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"></div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                    <div className="flex justify-center py-4 lg:pt-4 pt-8 sm:mt-12 mt-12 lg:mt-0">
                       <div className="mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
                           22
@@ -118,7 +105,8 @@ export default function Profile() {
                     </div>
                   </div>
                 </div>
-                <div className="text-center mt-12">
+
+                <div className="text-center mt-4">
                   <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
                     {name}
                   </h3>
@@ -138,12 +126,14 @@ export default function Profile() {
                 <div className="mt-10 py-10 border-t border-gray-300 text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
-                      <a
-                        href="#"
-                        className="font-normal text-purple-700 hover:purple-700"
+                      <button
+                        onClick={() => handleSignOut()}
+                        className="bg-purple-700 active:bg-purple-900 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:m-2 mb-1 hover:bg-purple-700/90"
+                        type="button"
+                        style={{ transition: "all .15s ease" }}
                       >
-                        Show more
-                      </a>
+                        Sign out
+                      </button>
                     </div>
                   </div>
                 </div>
